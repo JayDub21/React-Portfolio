0bb0b475c1e74b2c811b0c3aaa122ed88050fd24
JayDub21 <justin.wofford.21@gmail.com>
2020-02-28 10:21:13 -0600

    Updates

---
+++ precache-manifest.f2421a3f0efbea65a9d9a8e7561b6ae1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c87b9077b2306bf46d6b20eea438d8",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "612042eb982df605e13b",
+    "url": "/React-Portfolio/static/css/main.dc5315d4.chunk.css"
+  },
+  {
+    "revision": "f605bfefbbd9ed7d6751",
+    "url": "/React-Portfolio/static/js/2.b4c86509.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/React-Portfolio/static/js/2.b4c86509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "612042eb982df605e13b",
+    "url": "/React-Portfolio/static/js/main.68ea1e96.chunk.js"
+  },
+  {
+    "revision": "0bcb88620496209bc00d",
+    "url": "/React-Portfolio/static/js/runtime-main.61f444e2.js"
+  },
+  {
+    "revision": "59edf9ba4e7efabbbf73a68c914f38e8",
+    "url": "/React-Portfolio/static/media/employee-tracker.59edf9ba.gif"
+  },
+  {
+    "revision": "af65e0f70be7ae50527b89ec80a6d62a",
+    "url": "/React-Portfolio/static/media/nt-action.af65e0f7.png"
+  },
+  {
+    "revision": "48c86430dec486fd897863ca69c98a57",
+    "url": "/React-Portfolio/static/media/profile-gen.48c86430.png"
+  },
+  {
+    "revision": "ae530cfa478f6360cd97c9aac604197d",
+    "url": "/React-Portfolio/static/media/self.ae530cfa.jpg"
+  },
+  {
+    "revision": "6e9ca451d2ea93fe42ec2b20c3549ced",
+    "url": "/React-Portfolio/static/media/teamGen.6e9ca451.gif"
+  },
+  {
+    "revision": "4a74fcac8f9c146b83bf34bdba4caf0c",
+    "url": "/React-Portfolio/static/media/wanderdog.4a74fcac.png"
+  },
+  {
+    "revision": "9099704434ce0b616d97cce14399e461",
+    "url": "/React-Portfolio/static/media/webb.90997044.png"
+  },
+  {
+    "revision": "22664acb7c6790b4e08230ed04f61496",
+    "url": "/React-Portfolio/static/media/wuphf.22664acb.png"
+  }
+]);
